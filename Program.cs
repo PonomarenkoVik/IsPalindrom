f4957918b6074b8d7e5a0c16b024e218bb8aab9c
PonomarenkoVik <ponomarenkovic@gmail.com>
2018-07-20 20:29:37 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckStringByPal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validator val = new Validator();
+            string str = "";
+            string str1 = "g";
+            string pal = "34th";
+            int numCharPal;
+            bool result = val.CheckStringByPal(str, pal, out numCharPal);
+
+            Console.WriteLine(" String  -  {0}\n Noise  -  {1}\n Number of noise char  -  {2} \n Result  - {3}", str, pal, numCharPal, result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Validator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckStringByPal
+{
+    class Validator
+    {
+        public bool CheckStringByPal(string str, string pal)
+        {
+            int numNoiseChar = 0;
+
+            if (str == null)
+            {
+                return false;
+            }
+
+            bool result = true;
+            
+            int leftIndex = 0;
+            int rightIndex = str.Length - 1;
+            while (leftIndex < rightIndex)
+            {
+                if (IsNoise(str[leftIndex], pal))
+                {
+                    numNoiseChar++;
+                    leftIndex++;
+                    continue;
+                }
+
+                if (IsNoise(str[rightIndex], pal))
+                {
+                    numNoiseChar++;
+                    rightIndex--;
+                    continue;
+                }
+
+                if (str[leftIndex] != str[rightIndex])
+                {
+                    result = false;
+                    break;
+                }
+
+                leftIndex++;
+                rightIndex--;
+            }
+               
+                if (numNoiseChar == str.Length)
+                {
+                    result = false;
+                }               
+            return result;
+        }
+
+
+
+
+        private bool IsNoise(char ch, string pal)
+        {
+            if ((pal == null) && (pal == ""))
+            {
+                return false;
+            }
+
+            bool result = false;
+
+            for (int i = 0; i < pal.Length; i++)
+            {
+                if (ch == pal[i])
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
